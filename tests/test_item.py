77e7750328ec509134a78558c62fae75373eb06c
leon <leon.vladykin2288@gmail.com>
2024-01-24 17:44:52 +0500

    dz_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -11,8 +11,3 @@ def test_apply_discount():
     Item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 8000.0
-
-def test_all_items():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-    assert Item.all == [item1, item2]
