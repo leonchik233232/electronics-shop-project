1958e84fea56af7910f78676a5d221bc86bafb51
leon <leon.vladykin2288@gmail.com>
2024-02-21 15:59:49 +0500

    dz_6

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,22 +1,14 @@
-import pytest
+
 
 from src.Keyboard import Keyboard
 
-def test_instantiate_from_csv_valid():
-    file_path = BASE_DIR.joinpath('src', 'items.csv')
+kb = Keyboard('White Project 007', 5000, 3)
 
-    with open(file_path, 'r') as f:
-        keyboard = Keyboard.instantiate_from_csv(f)
+assert str(kb) == 'White Project 007'
 
-    assert len(keyboard.items) == 3
-
-def test_instantiate_from_csv_invalid():
-    invalid_file_path = BASE_DIR.joinpath('src', 'items_error.csv')
-
-    if os.path.exists(invalid_file_path):
-        with open(invalid_file_path, 'r') as f:
-            with pytest.raises(InstantiateCSVError):
-                Keyboard.instantiate_from_csv(f)
-    else:
-        with pytest.raises(FileNotFoundError):
-            Keyboard.instantiate_from_csv(invalid_file_path)
+kb.change_lang()
+assert str(kb.language) == 'RU'
+kb.change_lang()
+assert str(kb.language) == 'EN'
+kb.change_lang()
+assert str(kb.language) == 'RU'
